2ea02d26472bf8a3dc0548c53cce37e84da0c344
angle-droit <axelmertz90@gmail.com>
2025-10-29 19:27:22 +0100

    Styles NeoButton and NeoInput components
    Enhances the visual appearance of the NeoButton and
    NeoInput components by applying styling configurations.
    
    Adds background and foreground colors and borders to
    improve user experience.

--- src/com/alex/neoswing/component/NeoButton.java
+++ src/com/alex/neoswing/component/NeoButton.java
@@ -1,6 +1,7 @@
 package com.alex.neoswing.component;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 
 import java.awt.*;
 import java.awt.event.ActionListener;
@@ -12,6 +13,13 @@ public class NeoButton extends JButton {
 		super(text);
 		setPreferredSize(new Dimension(width, height));
 		setFont(new Font("Segoe UI", Font.BOLD, 14));
+		setBackground(new Color(255, 60, 0));
+		setForeground(new Color(255, 255, 255));
+		setBorder(new LineBorder(Color.BLACK, 1));
+		setBounds(30, 25, height, height);
+		setFocusPainted(false);
+		setOpaque(true);
+		setContentAreaFilled(true);
 		setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));	
 	}
 	
--- src/com/alex/neoswing/component/NeoInput.java
+++ src/com/alex/neoswing/component/NeoInput.java
@@ -1,15 +1,20 @@
 package com.alex.neoswing.component;
 
 import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
 
 @SuppressWarnings("serial")
 public class NeoInput extends JTextField {
 
-	public NeoInput(String text) {
+	public NeoInput(String text, int width, int height) {
 		super(text);
+		setPreferredSize(new Dimension(width, height));
 		setBackground(new Color(31, 31, 31));
+		setForeground(new Color(255, 255, 255));
+		setBorder(new LineBorder(Color.DARK_GRAY));
 	}
 
 	public String getInput() {
